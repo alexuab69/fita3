7ab344e1bf8d174b13c9d11d93242e2505eeef49
Mario Lacueva <mariolacueva01@gmail.com>
2025-01-09 13:31:34 +0100

    future trees

--- building_app/lib/screen_partition.dart
+++ building_app/lib/screen_partition.dart
@@ -4,6 +4,7 @@ import 'screen_space.dart';
 import 'main.dart';
 import 'dart:io';
 import 'data.dart';
+import 'requests.dart';
 
 class PartitionScreen extends StatefulWidget {
   final String id;
@@ -14,43 +15,59 @@ class PartitionScreen extends StatefulWidget {
 }
 
 class _PartitionScreenState extends State<PartitionScreen> {
-  late Tree tree;
+  late Future<Tree> futureTree;
 
   @override
   void initState() {
     super.initState();
-    tree = getTree(widget.id);
+    futureTree = getTree(widget.id);
   }
 
+  // future with listview
+  // https://medium.com/nonstopio/flutter-future-builder-with-list-view-builder-d7212314e8c9
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        foregroundColor: Theme.of(context).colorScheme.onPrimary, 
-        title: Text(tree.root.id),
-        actions: <Widget>[
-          IconButton(
-            icon: Image(
-              image: FileImage(File(Data.images['home_screen'] ?? 'images/default_image.png')),
+    return FutureBuilder<Tree>(
+      future: futureTree,
+      builder: (context, snapshot) {
+        // anonymous function
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              title: Text(snapshot.data!.root.id),
+              actions: <Widget>[
+                IconButton(icon: const Icon(Icons.home), onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Home Page')),
+                    );}
+                    ),
+                //TODO other actions
+              ],
             ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Home Page')),
-              );
-            },
-          ),
-        ],
-      ),
-      body: ListView.separated(
-        padding: const EdgeInsets.all(16.0),
-        itemCount: tree.root.children.length,
-        itemBuilder: (BuildContext context, int index) =>
-          _buildRow(tree.root.children[index], index),
-        separatorBuilder: (BuildContext context, int index) =>
-        const Divider(),
-      ),
+            body: ListView.separated(
+              // it's like ListView.builder() but better because it includes a separator between items
+              padding: const EdgeInsets.all(16.0),
+              itemCount: snapshot.data!.root.children.length,
+              itemBuilder: (BuildContext context, int i) =>
+                  _buildRow(snapshot.data!.root.children[i], i),
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        // By default, show a progress indicator
+        return Container(
+            height: MediaQuery.of(context).size.height,
+            color: Colors.white,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ));
+      },
     );
   }
 
